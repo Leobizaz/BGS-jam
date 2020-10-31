b7bad304760f47fb20bf7fe854f1732e96080458
Leonardo Biz Villar <leobizaz12@gmail.com>
2020-10-31 04:05:30 -0300

    Upada pra ficar safe

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -7,9 +7,12 @@ public class ShipController : MonoBehaviour
     //variaveis publicas
     public float forwardSpeed = 1;
     public float breakDecceleration = 1;
-    public float rotationDamp = 0;
+    public float rotationSpeed = 150;
     public float maxVelocity = 5;
-    
+    public Color32 cageColor;
+    public Color32 CageDangerColor;
+    [SerializeField] public static int currentCapacity = 0;
+
     //variaveis privadas
     [SerializeField] private float currentVelocity;
     [SerializeField] private float cursorX;
@@ -18,29 +21,35 @@ public class ShipController : MonoBehaviour
     private float yVelocity;
     private float xVelocity;
     private float zVelocity;
-
+    private bool isMoving;
 
     //referencia publica
+    public ParticleSystem CageWall;
+    public ParticleSystem CageProjection;
+    public ParticleSystem FX_Thruster;
     public GameObject AreaClamp;
     public GameObject cursor;
     public GameObject CM_targetGroup;
     //referencias privadas
     Rigidbody2D rb;
-
+    HingeJoint2D GravAnchorHinge;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        GravAnchorHinge = GetComponent<HingeJoint2D>();
     }
 
     private void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            AimAtCursor();
-            FreeMovement();
+            isMoving = true;
+            FX_Thruster.Play();
         }
         else
         {
+            FX_Thruster.Stop();
+            isMoving = false;
             Freios();
 
             cursor.transform.position = transform.position;
@@ -50,12 +59,34 @@ public class ShipController : MonoBehaviour
         MovementClamp();
     }
 
+    private void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            AimAtCursor();
+            FreeMovement();
+
+        }
+
+        HingeClamp();
+    }
+
+    void HingeClamp()
+    {
+    }
+
     void FreeMovement()
     {
         cursorDistance = Vector2.Distance(cursor.transform.position, this.transform.position);
-        rb.AddRelativeForce(Vector3.up * forwardSpeed * cursorDistance);
+        cursorDistance = Mathf.Clamp(cursorDistance, 0.1f, 30f);
+        rb.AddRelativeForce(Vector3.up * forwardSpeed * (cursorDistance*2));
         currentVelocity = rb.velocity.magnitude;
         rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -5, 5), Mathf.Clamp(rb.velocity.y, -5, 5));
+
+        //damp
+
+        rb.velocity = new Vector2(Mathf.SmoothDamp(rb.velocity.x, 0, ref xVelocity, breakDecceleration), Mathf.SmoothDamp(rb.velocity.y, 0, ref yVelocity, breakDecceleration));
+
     }
 
     void AimAtCursor()
@@ -66,7 +97,8 @@ public class ShipController : MonoBehaviour
         var offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
         var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
 
-        transform.rotation = Quaternion.Euler(0, 0, Mathf.SmoothDamp(transform.localRotation.eulerAngles.z, angle - 90, ref zVelocity, rotationDamp));
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, angle - 90), Time.deltaTime * rotationSpeed);
+        //transform.rotation = Quaternion.Euler(0, 0, 0);
 
         cursorX = Input.mousePosition.x;
         cursorY = Input.mousePosition.y;
---
+++ Assets/Step1Cargo.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Step1Cargo : MonoBehaviour
+{
+    public GameObject CageCenter;
+    bool picked;
+    public Collider2D col;
+    bool beingPicked;
+    float xVelocity;
+    float yVelocity;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Cursor" && !picked)
+        {
+            picked = true;
+            PickUP();
+        }
+
+        if (collision.gameObject.tag == "CageCenter")
+        {
+            beingPicked = false;
+            col.enabled = true;
+            gameObject.layer = 14;
+        }
+    }
+
+    private void Update()
+    {
+        if (picked)
+        {
+            if (Vector2.Distance(CageCenter.transform.position, this.transform.position) > 5)
+            {
+                picked = false;
+                ShipController.currentCapacity--;
+                gameObject.layer = 0;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (beingPicked)
+        {
+            transform.position = new Vector2(
+                Mathf.SmoothDamp(transform.position.x, CageCenter.transform.position.x, ref xVelocity, 0.3f), 
+                Mathf.SmoothDamp(transform.position.y, CageCenter.transform.position.y, ref yVelocity, 0.3f));
+        }
+    }
+
+    void PickUP()
+    {
+        ShipController.currentCapacity++;
+        beingPicked = true;
+        col.enabled = false;
+    }
+
+}
