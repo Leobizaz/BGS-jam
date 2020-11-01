973cd1b535b87409528789f65e210786091f40e2
Leonardo Biz Villar <leobizaz12@gmail.com>
2020-11-01 14:57:13 -0300

    Fase 1 sonorizada

---
+++ Assets/Playsound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playsound : MonoBehaviour
+{
+    public bool onEnable;
+    AudioSource audioS;
+    private void OnEnable()
+    {
+        if (onEnable)
+        {
+            audioS = GetComponent<AudioSource>();
+            audioS.Play();
+        }
+    }
+
+    public void PlaySound()
+    {
+        audioS = GetComponent<AudioSource>();
+        audioS.Play();
+    }
+}
