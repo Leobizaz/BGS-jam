51bc984bf0d426ba7ad02b78ebaf7c8239b5475a
Leonardo Biz Villar <leobizaz12@gmail.com>
2020-11-01 04:33:35 -0300

    LUVINHAS

---
+++ Assets/Pickup.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public PlayerController player;
+    public GameObject indicator;
+    bool onArea;
+    bool once;
+
+
+    void Start()
+    {
+        indicator.SetActive(false);
+    }
+    void Update()
+    {
+        if (onArea && Input.GetKeyDown(KeyCode.E) && !once)
+        {
+            Execute();
+
+        }
+    }
+
+    public void Execute()
+    {
+        if (!player.handsBusy)
+        {
+            Debug.Log("Pegou");
+            indicator.SetActive(false);
+            player.HoldObject(this.gameObject);
+        }
+        else
+        {
+            player.DropObject();
+            indicator.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            onArea = true;
+            indicator.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            indicator.SetActive(false);
+            onArea = false;
+        }
+    }
+}
